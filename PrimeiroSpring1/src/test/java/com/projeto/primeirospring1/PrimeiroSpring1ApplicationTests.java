0f5e07469d7b53462a0bc3aa9d63648f2fc741cc
GustavoTavares1 <104095093+GustavoTavares1@users.noreply.github.com>
2023-05-25 16:28:28 -0300

    Add files via upload

---
+++ PrimeiroSpring1/src/test/java/com/projeto/primeirospring1/PrimeiroSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.primeirospring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
